240ae5e94cef0937e7904d3d57a0ecf73c308b95
pomdtr <achille.lacoin@dailymotion.com>
2022-10-24 10:34:38 +0200

    remove unused struct

--- sunbeam/response.go
+++ sunbeam/response.go
@@ -1,18 +1,5 @@
 package sunbeam
 
-type FormResponse struct {
-	Title  string     `json:"title"`
-	Method string     `json:"dest" validate:"oneof=args stdin"`
-	Items  []FormItem `json:"items"`
-}
-
-type FormItem struct {
-	Type    string `json:"type" validate:"required,oneof=text password"`
-	Id      string `json:"id" validate:"required"`
-	Name    string `json:"name" validate:"required"`
-	Default string `json:"value"`
-}
-
 type ListItem struct {
 	Icon     string         `json:"icon"`
 	Title    string         `json:"title" validate:"required"`
