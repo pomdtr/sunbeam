f1c57ced1151636f7d313c222859a395b9a6056f
pomdtr <achille.lacoin@dailymotion.com>
2022-10-26 11:52:50 +0200

    update devcontainer config

--- .devcontainer/on-create.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -x
-set -euo pipefail
-
-npm install -g zx
--- replit.nix
+++
@@ -1,7 +0,0 @@
-{ pkgs }: {
-    deps = [
-        pkgs.go_1_18
-        pkgs.gopls
-        pkgs.nodePackages.zx
-    ];
-}
