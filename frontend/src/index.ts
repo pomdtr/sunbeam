d5382908eeb2e343f572bc0493856078cd880305
pomdtr <achille.lacoin@gmail.com>
2022-12-17 16:02:28 +0100

    use Tomorrow Night as the default theme

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -2,10 +2,21 @@ import { Terminal } from "xterm";
 import { FitAddon } from "xterm-addon-fit";
 import { CanvasAddon } from "xterm-addon-canvas";
 import { AttachAddon } from "xterm-addon-attach";
+import defaultTheme from "./theme.json";
+
+async function loadTheme() {
+  const res = await fetch("/theme.json");
+  if (res.ok) {
+    return res.json();
+  } else {
+    const error = await res.text();
+    console.error("Failed to load theme", error);
+    return defaultTheme;
+  }
+}
 
 async function main() {
-  const res = await fetch("/theme.json");
-  const theme = await res.json();
+  const theme = await loadTheme();
 
   const terminal = new Terminal({
     cursorBlink: true,
@@ -14,7 +25,7 @@ async function main() {
     fontSize: 13,
     scrollback: 0,
     fontFamily: "Consolas,Liberation Mono,Menlo,Courier,monospace",
-    theme: theme,
+    theme,
   });
 
   const ws = new WebSocket(`ws://${location.host}/ws`);
