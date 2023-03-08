01f408dee943d9dba0c87c7790901fe5050b471d
pomdtr <achille.lacoin@gmail.com>
2023-03-08 18:37:57 +0100

    add check flag

--- cmd/sunbeam.go
+++ cmd/sunbeam.go
@@ -1,51 +1,65 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
-	"io"
 	"os"
+	"os/exec"
 
 	"github.com/spf13/cobra"
 
+	"github.com/pomdtr/sunbeam/scripts"
 	"github.com/pomdtr/sunbeam/tui"
 )
 
 func Execute(version string) (err error) {
 	// rootCmd represents the base command when called without any subcommands
 	var rootCmd = &cobra.Command{
-		Use:     "sunbeam <script>",
-		Short:   "Command Line Launcher",
+		Use:   "sunbeam <script>",
+		Short: "Command Line Launcher",
+		Long: `Sunbeam is a command line launcher for your terminal, inspired by fzf and raycast.
+
+You will need to provide a compatible script as the first argument to you use sunbeam. See http://pomdtr.github.io/sunbeam for more information.`,
+		Args:    cobra.MinimumNArgs(1),
 		Version: version,
-		RunE: func(cmd *cobra.Command, args []string) (err error) {
+		Run: func(cmd *cobra.Command, args []string) {
 			var runner *tui.CommandRunner
-			if len(args) == 0 {
-				tempfile, err := os.CreateTemp("", "sunbeam_input.json")
-				if err != nil {
-					fmt.Fprintf(os.Stderr, "Failed to create tempfile: %v", err)
-					os.Exit(1)
-				}
-				defer os.Remove(tempfile.Name())
-
-				_, err = io.Copy(tempfile, os.Stdin)
-				if err != nil {
-					fmt.Fprintf(os.Stderr, "Failed to copy stdin to tempfile: %v", err)
-					os.Exit(1)
-				}
-
-				runner = tui.NewCommandRunner("cat", tempfile.Name())
-			} else {
-				command := args[0]
-				var commandArgs []string
-				if len(args) > 1 {
-					commandArgs = args[1:]
-				}
-				runner = tui.NewCommandRunner(command, commandArgs...)
+			command := args[0]
+			var commandArgs []string
+			if len(args) > 1 {
+				commandArgs = args[1:]
 			}
 
+			if check, _ := cmd.Flags().GetBool("check"); check {
+				cmd := exec.Command(command, commandArgs...)
+				output, err := cmd.Output()
+				if err != nil {
+					fmt.Println("An error occured while running the script:", err)
+					os.Exit(1)
+				}
+
+				var v interface{}
+				if err := json.Unmarshal(output, &v); err != nil {
+					fmt.Println("Script is not valid:", err)
+					os.Exit(1)
+				}
+
+				if err := scripts.Schema.Validate(v); err != nil {
+					fmt.Println("Script is not valid:", err)
+					os.Exit(1)
+				}
+
+				fmt.Println("Script is valid!")
+				os.Exit(0)
+			}
+
+			runner = tui.NewCommandRunner(command, commandArgs...)
 			model := tui.NewModel(runner)
-			return tui.Draw(model)
+			tui.Draw(model)
 		},
 	}
+
+	rootCmd.Flags().Bool("check", false, "Check if the script is valid")
 
 	return rootCmd.Execute()
 }
