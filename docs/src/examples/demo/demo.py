8febd52d2301e92d80a5913126c75ae076d7dff9
pomdtr <achille.lacoin@gmail.com>
2023-03-10 23:07:14 +0100

    reorder demo example to make it more readable

--- docs/src/examples/demo/demo.py
+++ docs/src/examples/demo/demo.py
@@ -4,6 +4,19 @@ import argparse
 import json
 import sys
 
+parser = argparse.ArgumentParser()
+
+subparsers = parser.add_subparsers(dest="command")
+
+static_list = subparsers.add_parser("static-list")
+static_list.add_argument("nb_items", type=int)
+
+dynamic_list = subparsers.add_parser("dynamic-list")
+
+help = subparsers.add_parser("help")
+
+args = parser.parse_args()
+
 welcome_text = """
 Welcome to sunbeam !
 
@@ -22,162 +35,145 @@ I support:
 Now hit escape to go back to the previous view.
 """
 
-
-def handle_args(args):
-    if not args.command:
-        output_as_json(
-            {
-                "type": "detail",
-                "content": {
-                    "text": welcome_text,
+if not args.command:
+    json.dump(
+        {
+            "type": "detail",
+            "content": {
+                "text": welcome_text,
+            },
+            "actions": [
+                {
+                    "type": "run",
+                    "onSuccess": "push",
+                    "title": "Tell me more about you!",
+                    "command": [sys.argv[0], "help"],
                 },
-                "actions": [
-                    {
-                        "type": "run",
-                        "onSuccess": "push",
-                        "title": "Tell me more about you!",
-                        "command": [sys.argv[0], "help"],
-                    },
-                    {
-                        "type": "run",
-                        "onSuccess": "push",
-                        "title": "Show me a static list!",
-                        "command": [sys.argv[0], "static-list", "${input:nb_items}"],
-                        "inputs": [
-                            {
-                                "name": "nb_items",
-                                "type": "textfield",
-                                "title": "Number of items to show",
-                            }
-                        ],
-                    },
-                    {
-                        "type": "run",
-                        "onSuccess": "push",
-                        "title": "Show me a dynamic list!",
-                        "command": [sys.argv[0], "dynamic-list"],
-                    },
-                    {
-                        "type": "open",
-                        "title": "Open the Docs",
-                        "target": "https://pomdtr.github.io/sunbeam/",
-                    },
-                    {
-                        "type": "copy",
-                        "title": "Copy list alias",
-                        "text": " ".join(
-                            ["sunbeam", "run", sys.argv[0], "static-list", "5"]
-                        ),
-                    },
-                ],
-            }
-        )
-    elif args.command == "help":
-        output_as_json(
-            {
-                "type": "detail",
-                "content": {
-                    "text": help_page,
+                {
+                    "type": "run",
+                    "onSuccess": "push",
+                    "title": "Show me a static list!",
+                    "command": [sys.argv[0], "static-list", "${input:nb_items}"],
+                    "inputs": [
+                        {
+                            "name": "nb_items",
+                            "type": "textfield",
+                            "title": "Number of items to show",
+                        }
+                    ],
                 },
-            }
-        )
-    elif args.command == "dynamic-list":
-        query = sys.stdin.read()
-        output_as_json(
-            {
-                "type": "list",
-                "generateItems": True,
-                "items": [
-                    {
-                        "title": query.upper(),
-                        "subtitle": "Upper case",
-                        "actions": [
-                            {
-                                "type": "copy",
-                                "text": query.upper(),
-                            }
-                        ],
+                {
+                    "type": "run",
+                    "onSuccess": "push",
+                    "title": "Show me a dynamic list!",
+                    "command": [sys.argv[0], "dynamic-list"],
+                },
+                {
+                    "type": "open",
+                    "title": "Open the Docs",
+                    "target": "https://pomdtr.github.io/sunbeam/",
+                },
+                {
+                    "type": "copy",
+                    "title": "Copy list alias",
+                    "text": " ".join(
+                        ["sunbeam", "run", sys.argv[0], "static-list", "5"]
+                    ),
+                },
+            ],
+        },
+        sys.stdout,
+    )
+elif args.command == "help":
+    json.dump(
+        {
+            "type": "detail",
+            "content": {
+                "text": help_page,
+            },
+        },
+        sys.stdout,
+    )
+elif args.command == "dynamic-list":
+    query = sys.stdin.read()
+    json.dump(
+        {
+            "type": "list",
+            "generateItems": True,
+            "items": [
+                {
+                    "title": query.upper(),
+                    "subtitle": "Upper case",
+                    "actions": [
+                        {
+                            "type": "copy",
+                            "text": query.upper(),
+                        }
+                    ],
+                },
+                {
+                    "title": query.lower(),
+                    "subtitle": "Lower case",
+                    "actions": [
+                        {
+                            "type": "copy",
+                            "text": query.lower(),
+                        }
+                    ],
+                },
+                {
+                    "title": query.title(),
+                    "subtitle": "Title case",
+                    "actions": [
+                        {
+                            "type": "copy",
+                            "text": query.title(),
+                        }
+                    ],
+                },
+                {
+                    "title": query.capitalize(),
+                    "subtitle": "Capitalize",
+                    "actions": [
+                        {
+                            "type": "copy",
+                            "text": query.capitalize(),
+                        }
+                    ],
+                },
+                {
+                    "title": query.swapcase(),
+                    "subtitle": "Swap case",
+                    "actions": [
+                        {
+                            "type": "copy",
+                            "text": query.swapcase(),
+                        }
+                    ],
+                },
+            ]
+            if query
+            else [{"title": "Please enter a query"}],
+        },
+        sys.stdout,
+    )
+elif args.command == "static-list":
+    json.dump(
+        {
+            "type": "list",
+            "showPreview": True,
+            "items": [
+                {
+                    "title": "Title",
+                    "subtitle": "Subtitle",
+                    "accessories": [f"Item {i+1}"],
+                    "preview": {
+                        "text": f"Preview text of item {i+1}",
                     },
-                    {
-                        "title": query.lower(),
-                        "subtitle": "Lower case",
-                        "actions": [
-                            {
-                                "type": "copy",
-                                "text": query.lower(),
-                            }
-                        ],
-                    },
-                    {
-                        "title": query.title(),
-                        "subtitle": "Title case",
-                        "actions": [
-                            {
-                                "type": "copy",
-                                "text": query.title(),
-                            }
-                        ],
-                    },
-                    {
-                        "title": query.capitalize(),
-                        "subtitle": "Capitalize",
-                        "actions": [
-                            {
-                                "type": "copy",
-                                "text": query.capitalize(),
-                            }
-                        ],
-                    },
-                    {
-                        "title": query.swapcase(),
-                        "subtitle": "Swap case",
-                        "actions": [
-                            {
-                                "type": "copy",
-                                "text": query.swapcase(),
-                            }
-                        ],
-                    },
-                ]
-                if query
-                else [{"title": "Please enter a query"}],
-            }
-        )
-    elif args.command == "static-list":
-        output_as_json(
-            {
-                "type": "list",
-                "showPreview": True,
-                "items": [
-                    {
-                        "title": "Title",
-                        "subtitle": "Subtitle",
-                        "accessories": [f"Item {i+1}"],
-                        "preview": {
-                            "text": f"Preview text of item {i+1}",
-                        },
-                        "actions": [],
-                    }
-                    for i in range(args.nb_items)
-                ],
-            }
-        )
-
-
-def output_as_json(data):
-    json.dump(data, sys.stdout)
-
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-
-    subparsers = parser.add_subparsers(dest="command")
-
-    static_list = subparsers.add_parser("static-list")
-    static_list.add_argument("nb_items", type=int)
-
-    dynamic_list = subparsers.add_parser("dynamic-list")
-
-    help = subparsers.add_parser("help")
-
-    handle_args(parser.parse_args())
+                    "actions": [],
+                }
+                for i in range(args.nb_items)
+            ],
+        },
+        sys.stdout,
+    )
