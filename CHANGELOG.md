6482e34cef61e6a3839e3b6398e7eba557920911
pomdtr <achille.lacoin@gmail.com>
2023-03-10 08:54:45 +0100

    autogenerate default config

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,12 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [0.2.2] - 2023-03-10
+
+### Added
+
+- Automatically generate the global config file if it doesn't exist, with a link to the documentation.
+
 ## [0.2.1] - 2023-03-10
 
 ### Added
--- cmd/sunbeam.go
+++ cmd/sunbeam.go
@@ -6,10 +6,15 @@ import (
 	"path"
 	"strconv"
 
+	_ "embed"
+
 	"github.com/spf13/cobra"
 
 	"github.com/pomdtr/sunbeam/tui"
 )
+
+//go:embed sunbeam.json
+var defaultConfig []byte
 
 func Execute(version string) error {
 	// rootCmd represents the base command when called without any subcommands
@@ -21,6 +26,31 @@ func Execute(version string) error {
 You will need to provide a compatible script as the first argument to you use sunbeam. See http://pomdtr.github.io/sunbeam for more information.`,
 		Args:    cobra.NoArgs,
 		Version: version,
+		// If the config file does not exist, create it
+		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+			homedir, err := os.UserHomeDir()
+			if err != nil {
+				return fmt.Errorf("Could not find home directory: %w", err)
+			}
+
+			configDir := path.Join(homedir, ".config", "sunbeam")
+			sunbeamConfig := path.Join(configDir, "sunbeam.json")
+
+			// If the config file exists, do nothing
+			if _, err := os.Stat(sunbeamConfig); err == nil {
+				return nil
+			}
+
+			if err := os.MkdirAll(configDir, 0755); err != nil {
+				return fmt.Errorf("Could not create config directory: %w", err)
+			}
+
+			if err := os.WriteFile(sunbeamConfig, defaultConfig, 0644); err != nil {
+				return fmt.Errorf("Could not write default config: %w", err)
+			}
+
+			return nil
+		},
 		Run: func(cmd *cobra.Command, args []string) {
 			padding, _ := cmd.Flags().GetInt("padding")
 			maxHeight, _ := cmd.Flags().GetInt("height")
@@ -32,7 +62,12 @@ You will need to provide a compatible script as the first argument to you use su
 				}
 
 				runner := tui.NewCommandRunner(func(s string) ([]byte, error) {
-					return os.ReadFile(manifest)
+					bytes, err := os.ReadFile(manifest)
+					if err != nil {
+						return nil, fmt.Errorf("Could not read manifest: %w", err)
+					}
+
+					return bytes, nil
 				})
 				model := tui.NewModel(runner, tui.SunbeamOptions{
 					Padding:   padding,
