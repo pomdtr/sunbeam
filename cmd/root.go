90baf1551733a99a1fd878e245d517da670a0cc4
pomdtr <achille.lacoin@gmail.com>
2023-03-19 10:44:45 +0100

    improve root view

--- cmd/root.go
+++ cmd/root.go
@@ -9,19 +9,13 @@ import (
 
 	_ "embed"
 
-	"github.com/joho/godotenv"
 	"github.com/mattn/go-isatty"
 	"github.com/santhosh-tekuri/jsonschema/v5"
 	"github.com/spf13/cobra"
 
 	"github.com/pomdtr/sunbeam/tui"
+	"github.com/pomdtr/sunbeam/types"
 )
-
-//go:embed templates/root.yml
-var defaultManifest []byte
-
-//go:embed templates/.env
-var defaultEnv []byte
 
 func exitWithErrorMsg(msg string, args ...any) {
 	fmt.Fprintf(os.Stderr, msg, args...)
@@ -43,12 +37,9 @@ var (
 func Execute(version string, schema *jsonschema.Schema) error {
 	homeDir, _ := os.UserHomeDir()
 
-	configDir := path.Join(homeDir, ".config", "sunbeam")
 	dataDir := path.Join(homeDir, ".local", "share", "sunbeam")
 	extensionDir := path.Join(dataDir, "extensions")
 
-	rootFile := path.Join(configDir, "root.yml")
-	envFile := path.Join(configDir, ".env")
 	validator := func(page []byte) error {
 		var v any
 		if err := json.Unmarshal(page, &v); err != nil {
@@ -68,26 +59,56 @@ See https://pomdtr.github.io/sunbeam for more information.`,
 		Args:    cobra.NoArgs,
 		Version: version,
 		PreRun: func(cmd *cobra.Command, args []string) {
-			if _, err := os.Stat(rootFile); os.IsNotExist(err) {
-				// Create the config file
-				_ = os.MkdirAll(configDir, 0755)
-				// Write the default manifest
-				_ = os.WriteFile(rootFile, defaultManifest, 0644)
-			}
-
-			if _, err := os.Stat(envFile); os.IsNotExist(err) {
-				// Create the config file
-				_ = os.MkdirAll(configDir, 0755)
-				// Write the default manifest
-				_ = os.WriteFile(envFile, defaultEnv, 0644)
-			}
-
-			godotenv.Load(envFile)
 			os.Setenv("SUNBEAM", "1")
 		},
 		// If the config file does not exist, create it
 		Run: func(cmd *cobra.Command, args []string) {
-			generator := tui.NewFileGenerator(rootFile)
+			generator := func(string) ([]byte, error) {
+				items := []types.ListItem{
+					{
+						Title:    "Browse Available Extensions",
+						Subtitle: "Sunbeam",
+						Actions: []types.Action{
+							{Type: types.RunAction, OnSuccess: types.PushOnSuccess, Command: "sunbeam extension browse"},
+						},
+					},
+					{
+						Title:    "Create Extension",
+						Subtitle: "Sunbeam",
+						Actions: []types.Action{
+							{
+								Type:    types.RunAction,
+								Command: "sunbeam extension create ${input:name}",
+								Inputs: []types.Input{
+									{Name: "name", Type: types.TextFieldInput, Title: "Extension name", Placeholder: "Extension Name"},
+								},
+							},
+						},
+					},
+				}
+
+				extension, err := ListExtensions(extensionDir)
+				if err != nil {
+					return nil, fmt.Errorf("could not list extensions: %s", err)
+				}
+
+				for _, extension := range extension {
+					items = append(items, types.ListItem{
+						Title:    fmt.Sprintf("Run %s Extension", extension),
+						Subtitle: extension,
+						Actions: []types.Action{
+							{Type: types.RunAction, OnSuccess: types.PushOnSuccess, Command: "sunbeam " + extension},
+							{Type: types.RunAction, Title: "Upgrade", Shortcut: "ctrl+u", Command: fmt.Sprintf("sunbeam extension upgrade %s", extension)},
+							{Type: types.RunAction, Title: "Remove", Shortcut: "ctrl+x", Command: fmt.Sprintf("sunbeam extension remove %s", extension)},
+						},
+					})
+				}
+
+				return json.Marshal(types.Page{
+					Type:  types.ListPage,
+					Items: items,
+				})
+			}
 
 			if !isatty.IsTerminal(os.Stdout.Fd()) {
 				output, err := generator("")
