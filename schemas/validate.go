2c785dea8788801ed03d171889be5276246aab1b
pomdtr <achille@deta.sh>
2023-05-04 19:59:46 +0200

    simplify error message

--- schemas/validate.go
+++ schemas/validate.go
@@ -19,7 +19,15 @@ func Validate(b []byte) error {
 	}
 
 	if err := schema.Validate(v); err != nil {
-		return fmt.Errorf("invalid schema: %#v", err)
+		if ve, ok := err.(*jsonschema.ValidationError); ok {
+			leaf := ve
+			for len(leaf.Causes) > 0 {
+				leaf = leaf.Causes[0]
+			}
+			return fmt.Errorf("%s does not validate with sunbeam schema", leaf.InstanceLocation)
+		} else {
+			return fmt.Errorf("invalid schema: %s", err)
+		}
 	}
 
 	return nil
