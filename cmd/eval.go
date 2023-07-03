955051d5e5614dfee6a4afe9b59bcc7f0d286baf
pomdtr <achille@deta.sh>
2023-07-03 16:34:34 +0200

    allow eval command to take input from stdin

--- cmd/eval.go
+++ cmd/eval.go
@@ -1,6 +1,11 @@
 package cmd
 
 import (
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/mattn/go-isatty"
 	"github.com/pomdtr/sunbeam/internal"
 	"github.com/pomdtr/sunbeam/types"
 	"github.com/spf13/cobra"
@@ -10,10 +15,27 @@ func NewEvalCmd() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "eval <code>",
 		Short: "Evaluate code with val.town",
-		Args:  cobra.ExactArgs(1),
+		Args:  cobra.MaximumNArgs(1),
+		PreRunE: func(cmd *cobra.Command, args []string) error {
+			if len(args) == 0 && isatty.IsTerminal(os.Stdin.Fd()) {
+				return fmt.Errorf("no code provided")
+			}
+			return nil
+		},
 		RunE: func(cmd *cobra.Command, args []string) error {
+			var code string
+			if len(args) > 0 {
+				code = args[0]
+			} else {
+				bs, err := io.ReadAll(os.Stdin)
+				if err != nil {
+					return err
+				}
+				code = string(bs)
+			}
+
 			expression := types.Expression{
-				Code: args[0],
+				Code: code,
 			}
 			return Run(internal.NewRequestGenerator(expression.Request()))
 		},
