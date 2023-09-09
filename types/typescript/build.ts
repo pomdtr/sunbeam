7641a39e443a5fac076a1aaee69210ea24195831
pomdtr <achille.lacoin@gmail.com>
2023-09-09 13:00:16 +0200

    switch from node-fetch to undici

--- types/typescript/build.ts
+++ types/typescript/build.ts
@@ -14,33 +14,7 @@ await build({
   shims: {
     // see JS docs for overview and more options
     deno: true,
-    custom: [
-      {
-        package: {
-          name: "node-fetch",
-          version: "~3.3.2",
-        },
-        globalNames: [
-          {
-            // for the `fetch` global...
-            name: "fetch",
-            // use the default export of node-fetch
-            exportName: "default",
-          },
-          {
-            // for the `Response` global...
-            name: "Response",
-            exportName: "Response",
-          },
-          {
-            // for the `Response` global...
-            name: "Request",
-            exportName: "Request",
-            typeOnly: true,
-          },
-        ],
-      },
-    ],
+    undici: true,
   },
   package: {
     // package.json properties
