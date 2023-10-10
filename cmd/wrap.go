2b34bb5a99e68149d25801629691555113876ede
pomdtr <achille.lacoin@gmail.com>
2023-10-10 16:49:53 +0200

    simplify wrap command

--- cmd/wrap.go
+++ cmd/wrap.go
@@ -12,17 +12,16 @@ func NewCmdWrap() *cobra.Command {
 		Use:                "wrap",
 		Short:              "Wrap a command to be used interactively from a sunbeam extension",
 		Hidden:             true,
-		DisableFlagParsing: true,
 		Args:               cobra.MinimumNArgs(1),
+		DisableFlagParsing: true,
 		RunE: func(cmd *cobra.Command, args []string) error {
-			wrapped := exec.Command(args[0], args[1:]...)
-
-			f, err := os.Open("/dev/tty")
+			tty, err := os.Open("/dev/tty")
 			if err != nil {
 				return err
 			}
 
-			wrapped.Stdin = f
+			wrapped := exec.Command(args[0], args[1:]...)
+			wrapped.Stdin = tty
 			wrapped.Stdout = os.Stderr
 
 			return wrapped.Run()
