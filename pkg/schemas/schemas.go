6acee73eaf51e3a97f194aac830f41643c31deac
pomdtr <achille.lacoin@gmail.com>
2023-10-10 16:26:01 +0200

    improve error messages

--- pkg/schemas/schemas.go
+++ pkg/schemas/schemas.go
@@ -3,6 +3,7 @@ package schemas
 import (
 	"embed"
 	"encoding/json"
+	"fmt"
 
 	"github.com/santhosh-tekuri/jsonschema/v5"
 )
@@ -48,38 +49,38 @@ func init() {
 	}
 }
 
-func ValidatePage(input []byte) error {
+func formatValidationError(ve *jsonschema.ValidationError) string {
+	leaf := ve
+	for len(leaf.Causes) > 0 {
+		leaf = leaf.Causes[0]
+	}
+	return fmt.Sprintf("%s is not valid: %s", leaf.InstanceLocation, leaf.Message)
+}
+
+func validateSchema(schema string, input []byte) error {
 	var v interface{}
 	if err := json.Unmarshal(input, &v); err != nil {
 		return err
 	}
 
-	if err := schemas["page.schema.json"].Validate(v); err != nil {
+	if err := schemas[schema].Validate(v); err != nil {
+		if ve, ok := err.(*jsonschema.ValidationError); ok {
+			return fmt.Errorf(formatValidationError(ve))
+		}
 		return err
 	}
+
 	return nil
+}
+
+func ValidatePage(input []byte) error {
+	return validateSchema("page.schema.json", input)
 }
 
 func ValidateCommand(input []byte) error {
-	var v interface{}
-	if err := json.Unmarshal(input, &v); err != nil {
-		return err
-	}
-
-	if err := schemas["command.schema.json"].Validate(v); err != nil {
-		return err
-	}
-	return nil
+	return validateSchema("command.schema.json", input)
 }
 
 func ValidateManifest(input []byte) error {
-	var v interface{}
-	if err := json.Unmarshal(input, &v); err != nil {
-		return err
-	}
-
-	if err := schemas["manifest.schema.json"].Validate(v); err != nil {
-		return err
-	}
-	return nil
+	return validateSchema("manifest.schema.json", input)
 }
