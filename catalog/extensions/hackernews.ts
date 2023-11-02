ff8990d7c44bfbbc8536cd326b4be3d1baa8d7c0
pomdtr <achille.lacoin@gmail.com>
2023-11-03 00:42:31 +0100

    add hackernews script

---
+++ catalog/extensions/hackernews.ts
@@ -0,0 +1,66 @@
+#!/usr/bin/env deno run -A
+
+import Parser from "npm:rss-parser";
+import { formatDistance } from "npm:date-fns";
+import * as sunbeam from "npm:sunbeam-types@0.23.15"
+
+if (Deno.args.length == 0) {
+    const manifest: sunbeam.Manifest = {
+        title: "Hacker ",
+        description: "Manage your RSS feeds",
+        commands: [
+            {
+                name: "frontpage",
+                title: "Show a feed",
+                mode: "list",
+            },
+        ]
+    };
+
+    console.log(JSON.stringify(manifest));
+    Deno.exit(0);
+}
+
+const payload = JSON.parse(Deno.args[0]) as sunbeam.CommandInput;
+if (payload.command == "frontpage") {
+    const feed = await new Parser().parseURL("https://hnrss.org/frontpage?description=0&count=25");
+    const page: sunbeam.List = {
+        items: feed.items.map((item) => ({
+            title: item.title || "",
+            subtitle: item.categories?.join(", ") || "",
+            accessories: item.isoDate
+                ? [
+                    formatDistance(new Date(item.isoDate), new Date(), {
+                        addSuffix: true,
+                    }),
+                ]
+                : [],
+            actions: [
+                {
+                    title: "Open in browser",
+                    type: "open",
+                    target: item.link || "",
+                    exit: true
+                },
+                {
+                    title: "Open Comments in Browser",
+                    type: "open",
+                    target: item.guid || "",
+                    exit: true
+                },
+                {
+                    title: "Copy Link",
+                    type: "copy",
+                    key: "c",
+                    text: item.link || "",
+                    exit: true
+                },
+            ],
+        })),
+    };
+
+    console.log(JSON.stringify(page));
+} else {
+    console.error("Unknown command");
+    Deno.exit(1);
+}
