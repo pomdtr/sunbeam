02d5c8e7ea32551d4b0bfd7f075047c38908b647
pomdtr <achille.lacoin@gmail.com>
2023-11-03 12:24:07 +0100

    add ability to run arbitrary command from the root view

--- internal/tui/root.go
+++ internal/tui/root.go
@@ -62,6 +62,7 @@ func (c *RootList) Reload() tea.Msg {
 	c.environ = environ
 	c.extensions = extensionMap
 	c.history.Sort(rootItems)
+	c.list.SetEmptyText("No results found, hit alt+enter to run as shell command")
 	c.list.SetIsLoading(false)
 	c.list.SetItems(rootItems...)
 	return nil
@@ -100,6 +101,24 @@ func (c *RootList) Update(msg tea.Msg) (Page, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		switch msg.String() {
+		case "alt+enter":
+			if _, ok := c.list.Selection(); ok {
+				break
+			}
+
+			query := c.list.Query()
+			if query == "" {
+				break
+			}
+
+			cmd := exec.Command("sh", "-c", c.list.Query())
+			return c, tea.ExecProcess(cmd, func(err error) tea.Msg {
+				if err != nil {
+					return err
+				}
+
+				return ExitMsg{}
+			})
 		case "ctrl+e":
 			editor := utils.FindEditor()
 			editCmd := exec.Command("sh", "-c", fmt.Sprintf("%s %s", editor, config.Path()))
