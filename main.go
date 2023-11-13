cab5817ff423c75ec3739c17e08253269b1beb7d
pomdtr <achille.lacoin@gmail.com>
2023-11-13 19:44:07 +0100

    fix default config, add path check

--- main.go
+++ main.go
@@ -3,11 +3,17 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/exec"
 
 	"github.com/pomdtr/sunbeam/cmd"
 )
 
 func main() {
+	if _, err := exec.LookPath("sunbeam"); err != nil {
+		fmt.Fprintln(os.Stderr, "Please make sure that sunbeam is available in your $PATH")
+		os.Exit(1)
+	}
+
 	rootCmd, err := cmd.NewRootCmd()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "error: %s\n", err)
