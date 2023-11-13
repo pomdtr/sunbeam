4c068148b99b10965adbf77d43a9a7df16742bfd
pomdtr <achille.lacoin@gmail.com>
2023-11-14 00:33:45 +0100

    simplify install script

--- scripts/install.tmpl.sh
+++ scripts/install.tmpl.sh
@@ -7,81 +7,46 @@
 # Acknowledgments:
 #   - getmic.ro: https://github.com/benweissmann/getmic.ro
 
-set -e -u
+set -eu
 
 platform=''
 machine=$(uname -m)
-
-if [ "${GETSUNBEAM_PLATFORM:-x}" != "x" ]; then
-  platform="$GETSUNBEAM_PLATFORM"
-else
-  case "$(uname -s | tr '[:upper:]' '[:lower:]')" in
-    "linux")
-      case "$machine" in
-        "arm64"* | "aarch64"* ) platform='linux_arm64' ;;
-        "arm"* | "aarch"*) platform='linux_arm' ;;
-        *"86") platform='linux_386' ;;
-        *"64") platform='linux_amd64' ;;
-      esac
-      ;;
-    "darwin")
-      case "$machine" in
-        "arm64"* | "aarch64"* ) platform='darwin_arm64' ;;
-        *"64") platform='darwin_amd64' ;;
-      esac
-      ;;
-    "msys"*|"cygwin"*|"mingw"*|*"_nt"*|"win"*)
-      printf "Windows is not supported yet\n" >&2
-      exit 1
-      # case "$machine" in
-      #   *"86") platform='windows_386' ;;
-      #   *"64") platform='windows_amd64' ;;
-      # esac
-      ;;
-  esac
-fi
-
-if [ "x$platform" = "x" ]; then
-  cat << 'EOM'
-/=====================================\\
-|      COULD NOT DETECT PLATFORM      |
-\\=====================================/
-Uh oh! We couldn't automatically detect your operating system.
-To continue with installation, please choose from one of the following values:
-- linux_arm
-- linux_arm64
-- linux_386
-- linux_amd64
-- darwin_amd64
-- darwin_arm64
-Export your selection as the GETSUNBEAM_PLATFORM environment variable, and then
-re-run this script.
-For example:
-  $ export GETSUNBEAM_PLATFORM=linux_amd64
-  $ curl https://pomdtr.github.io/sunbeam/install.sh | bash
-EOM
-  exit 1
-else
-  printf "Detected platform: %s\n" "$platform"
-fi
-
-if [ "x$platform" = "xwindows_amd64" ] || [ "x$platform" = "xwindows_386" ]; then
-  extension='zip'
-else
-  extension='tar.gz'
-fi
-
-printf "Latest Version: %s\n" "TAG"
-printf "Downloading https://github.com/pomdtr/sunbeam/releases/download/%s/sunbeam-%s.%s\n" "TAG" "$platform" "$extension"
-
-curl -L "https://github.com/pomdtr/sunbeam/releases/download/TAG/sunbeam-$platform.$extension" > "sunbeam.$extension"
-
-case "$extension" in
-  "zip") unzip -j "sunbeam.$extension" -d "sunbeam" ;;
-  "tar.gz") tar -xvzf "sunbeam.$extension" "sunbeam" ;;
+case "$(uname -s | tr '[:upper:]' '[:lower:]')" in
+  "linux")
+    case "$machine" in
+      "arm64"* | "aarch64"* ) platform='linux_arm64' ;;
+      "arm"* | "aarch"*) platform='linux_arm' ;;
+      *"86") platform='linux_386' ;;
+      *"64") platform='linux_amd64' ;;
+    esac
+    ;;
+  "darwin")
+    case "$machine" in
+      "arm64"* | "aarch64"* ) platform='darwin_arm64' ;;
+      *"64") platform='darwin_amd64' ;;
+    esac
+    ;;
+  *)
+    printf "Platform not supported: %s\n" "$(uname -s)"
+    exit 1
+    ;;
 esac
 
-rm "sunbeam.$extension"
+TAG="{{TAG}}"
+printf "Target Version: %s\n" "$TAG"
+printf "Downloading https://github.com/pomdtr/sunbeam/releases/download/%s/sunbeam-%s.tar.gz\n" "$TAG" "$platform"
 
-mv sunbeam "${GETSUNBEAM_DEST:-/usr/local/bin/sunbeam}"
-echo "sunbeam installed to $GETSUNBEAM_DEST"
+temp=$(mktemp -d)
+trap 'rm -rf "$temp"' EXIT
+curl -L "https://github.com/pomdtr/sunbeam/releases/download/$TAG/sunbeam-$platform.tar.gz" > "$temp/sunbeam.tar.gz"
+
+printf "\nAdding sunbeam binary to %s\n" "/usr/local/bin"
+
+tar zxf "$temp/sunbeam.tar.gz" -C "$temp"
+if [ "$(id -u)" -ne 0 ]; then
+  sudo mv "$temp/sunbeam" "/usr/local/bin/sunbeam"
+else
+  mv "$temp/sunbeam" "/usr/local/bin/sunbeam"
+fi
+
+printf "\nDone! You can now run sunbeam.\n"
