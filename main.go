92f82e9e8bc201c4dfaa318863c4d20ea2c1cb26
pomdtr <achille.lacoin@gmail.com>
2023-11-13 23:39:10 +0100

    fix CI issues

--- main.go
+++ main.go
@@ -3,17 +3,11 @@ package main
 import (
 	"fmt"
 	"os"
-	"os/exec"
 
 	"github.com/pomdtr/sunbeam/cmd"
 )
 
 func main() {
-	if _, err := exec.LookPath("sunbeam"); err != nil {
-		fmt.Fprintln(os.Stderr, "Please make sure that sunbeam is available in your $PATH")
-		os.Exit(1)
-	}
-
 	rootCmd, err := cmd.NewRootCmd()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "error: %s\n", err)
