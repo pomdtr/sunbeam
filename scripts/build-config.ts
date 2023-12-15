c6d0624f292da84e196b9d724faf9bd004643cd3
pomdtr <achille.lacoin@gmail.com>
2023-12-15 16:47:18 +0100

    add schema to the config build script

--- scripts/build-config.ts
+++ scripts/build-config.ts
@@ -1,24 +1,26 @@
 #!/usr/bin/env -S deno run -A
-import * as sunbeam from "https://deno.land/x/sunbeam/mod.ts"
+import * as sunbeam from "https://deno.land/x/sunbeam/mod.ts";
 import * as path from "https://deno.land/std/path/mod.ts";
 
 const dirname = new URL(".", import.meta.url).pathname;
 
-const extensions: Record<string, sunbeam.ExtensionConfig> = {}
-const entries = Deno.readDirSync(path.join(dirname, "..", "extensions"))
+const extensions: Record<string, sunbeam.ExtensionConfig> = {};
+const entries = Deno.readDirSync(path.join(dirname, "..", "extensions"));
 for (const entry of entries) {
-    const extension = path.extname(entry.name);
-    const stem = path.basename(entry.name, extension);
+  const extension = path.extname(entry.name);
+  const stem = path.basename(entry.name, extension);
 
-
-    extensions[stem] = {
-        origin: `./extensions/${entry.name}`
-    }
+  extensions[stem] = {
+    origin: `./extensions/${entry.name}`,
+  };
 }
 
 const config: sunbeam.Config = {
-    extensions
-}
+  $schema: "./internal/schemas/config.schema.json",
+  extensions,
+};
 
-Deno.writeTextFileSync(path.join(dirname, "..", "sunbeam.json"), JSON.stringify(config, null, 2))
-
+Deno.writeTextFileSync(
+  path.join(dirname, "..", "sunbeam.json"),
+  JSON.stringify(config, null, 2)
+);
