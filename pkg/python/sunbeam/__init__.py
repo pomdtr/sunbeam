53b27d346dd82594b0dbe9662a352b9bec2b0c34
pomdtr <achille.lacoin@gmail.com>
2023-12-20 15:48:56 +0100

    limit the number of items exported by default

--- pkg/python/sunbeam/__init__.py
+++ pkg/python/sunbeam/__init__.py
@@ -1,6 +1,7 @@
 """Sunbeam Types"""
 
-from .manifest import *
-from .page import *
+from .manifest import Manifest, Command
+from .page import List, Detail, ListItem, Action
 
-__version__ = "0.2.3"
+__version__ = "0.2.4"
+__all__ = ["Manifest", "Command", "List", "Detail", "ListItem", "Action"]
