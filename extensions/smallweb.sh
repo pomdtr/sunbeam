7f1ad51a3be4c23ffe9d6fcd356727fd4d19234e
pomdtr <achille.lacoin@gmail.com>
2025-01-30 00:52:38 +0100

    add smallweb.sh script for searching and listing apps

---
+++ extensions/smallweb.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# Smallweb: https://smallweb.run
+
+if [ $# -eq 0 ]; then
+  jq -n '{
+    title: "SmallWeb",
+    root: [
+      { title: "Search Apps", type: "run", command: "search-apps" }
+    ],
+    commands: [
+      {
+        name: "search-apps",
+        mode: "filter"
+      }
+    ]
+  }'
+  exit 0
+fi
+
+if [ "$1" = "search-apps" ]; then
+    smallweb ls --json | jq 'map({
+        title: .name,
+        accessories: [.url],
+        actions: [
+            { title: "Open in Browser", type: "open", target: .url },
+            { title: "Open Dir", type: "open", target: .dir },
+            { title: "Copy URL", type: "copy", text: .url }
+        ]
+    }) | { items: . }'
+fi
